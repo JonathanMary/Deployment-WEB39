dea4bc4db33dc5bb4fd0cce8a89ac848f36a1213
JonathanMary <jon85ma@gmail.com>
2021-03-11 19:03:18 +0100

    wip

--- index.js
+++ index.js
@@ -12,6 +12,10 @@ if (process.env.NODE_ENV !== 'production') { //on Heroku, an env variable is cal
     server.use(cors());
 }
 
+server.use('*', (req, res) => {
+    res.send('<h1>It works!</h1>');
+})
+
 const PORT = process.env.PORT || 4000;
 server.listen(PORT, () => {
     console.log(`listening on ${PORT}`);
