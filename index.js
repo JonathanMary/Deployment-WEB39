eb20718b260cff8891e5fd0810bf4906953944fc
JonathanMary <jon85ma@gmail.com>
2021-03-11 18:43:54 +0100

    wip

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 console.log("It's ON!")
+require('dotenv').config();
+const express = require('express');
+
+const server = express();
+
+server.use(express.json());
+
+console.log(process.env.NODE_ENV)
+if (process.env.NODE_ENV !== 'production') { //on Heroku, an env variable is called "NODE_ENV" -> "production"
+    const cors = require('cors');
+    server.use(cors());
+}
